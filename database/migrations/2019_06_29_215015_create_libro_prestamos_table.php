4768223047039778a41a70970718b04966895ec6
Yeraida1710 <amontalban@uniguajira.edu.co>
2019-06-29 17:45:03 -0500

    error en relación de la tabla lobro prestamos

--- database/migrations/2019_06_29_215015_create_libro_prestamos_table.php
+++ database/migrations/2019_06_29_215015_create_libro_prestamos_table.php
@@ -16,7 +16,7 @@ class CreateLibroPrestamosTable extends Migration
         Schema::create('libro_prestamos', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id');
-            $table->foreign('user_id','fk_libroprestamos_usuarios')->references('id')->on('usuario')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('user_id','fk_libroprestamos_usuarios')->references('id')->on('usuarios')->onDelete('CASCADE')->onUpdate('CASCADE');
             $table->unsignedInteger('book_id');
             $table->foreign('book_id','fk_libroprestamos_libros')->references('id')->on('libros')->onDelete('CASCADE')->onUpdate('CASCADE');
             $table->date('loan_date');
